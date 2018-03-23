d94268c0275cdadef7624dc548100f5365626b2d
nicolasreymond <n.reymond.12@gmail.com>
2018-03-23 08:25:10 +0100

    WIP

--- Reymond_Nicolas_requetes_INFO1D_2018.sql
+++ Reymond_Nicolas_requetes_INFO1D_2018.sql
@@ -1,29 +1,29 @@
-DROP TABLE `T_Hadi`;
+  DROP TABLE `T_Hadi`;
 
-CREATE TABLE `T_Hadi` (`ID_Hadi` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, `nom_Hadi` varchar(25) DEFAULT NULL);
+  CREATE TABLE `T_Hadi` (`ID_Hadi` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, `nom_Hadi` varchar(25) DEFAULT NULL);
 
-INSERT INTO `T_Hadi` (`nom_Hadi`) VALUES ('toto'), ('root'), ('Malera'), ('Kali');
+  INSERT INTO `T_Hadi` (`nom_Hadi`) VALUES ('toto'), ('root'), ('Malera'), ('Kali');
 
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` = 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` < 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` > 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` <= 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` >= 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` != 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` = 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` < 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` > 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` <= 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` >= 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` != 2 ;
 
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` LIKE 2 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT LIKE 2 ;
-SELECT * FROM `T_Hadi` WHERE `nom_Hadi` LIKE "%r%" ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` LIKE 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT LIKE 2 ;
+  SELECT * FROM `T_Hadi` WHERE `nom_Hadi` LIKE "%r%" ;
 
-SELECT * FROM `T_Hadi` WHERE `nom_Hadi` ="root" ;
-SELECT * FROM `T_Hadi` WHERE `nom_Hadi` !="root" ;
+  SELECT * FROM `T_Hadi` WHERE `nom_Hadi` ="root" ;
+  SELECT * FROM `T_Hadi` WHERE `nom_Hadi` !="root" ;
 
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` IN (1, 3) ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT IN (2, 4) ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` IN (1, 3) ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT IN (2, 4) ;
 
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` BETWEEN 2 AND 3 ;
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT BETWEEN 1 AND 2 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` BETWEEN 2 AND 3 ;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` NOT BETWEEN 1 AND 2 ;
 
-SELECT * FROM `T_Hadi` WHERE `ID_Hadi` IS NOT NULL;
+  SELECT * FROM `T_Hadi` WHERE `ID_Hadi` IS NOT NULL;
 
-SELECT * FROM `T_Hadi` WHERE (`nom_Hadi` REGEXP '[Kt]\w*');
+  SELECT * FROM `T_Hadi` WHERE (`nom_Hadi` REGEXP '[Kt]\w*');
