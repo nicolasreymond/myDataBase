7acc55f6d66c00d9c1de7ae0ecb0fcb90e956fa7
nicolasreymond <n.reymond.12@gmail.com>
2018-05-25 09:22:15 +0200

    25.05.18

--- Reymond_Nicolas_requetes_INFO1D_2018.sql
+++ Reymond_Nicolas_requetes_INFO1D_2018.sql
@@ -53,7 +53,17 @@ SELECT DISTINCT `titre`, `ID_Genre`,`nom_genre` FROM `T_Video` T1
    LEFT JOIN `T_Genre` T3 ON T3.ID_Genre = T2.fk_Genre
    INNER JOIN `T_Produit` T4 ON T4.fk_Video = T1.ID_Video
    INNER JOIN `T_Realisateur` T5 ON T5.ID_Realisateur = T4.fk_Realisateur
-WHERE `ID_Video` = 2
-
+WHERE `ID_Video` = 2;
 
 SELECT * FROM `T_Hadi` ORDER BY `ID_Hadi` COLLATE latin1_german2_ci;
+
+SELECT (TIMEDIFF(duree, (SELECT duree FROM T_Video WHERE `ID_Video` = 1))) AS time FROM T_Video ;
+
+SELECT COUNT(*) AS nb_video FROM T_Video;
+SELECT AVG(duree) FROM T_Video WHERE duree IS NOT NULL;
+SELECT MAX(duree) FROM T_Video WHERE duree IS NOT NULL;
+SELECT MIN(date_sortie) FROM T_Video WHERE date_sortie IS NOT NULL;
+
+SELECT (SELECT COUNT(*) FROM T_Video) AS nb_video, (SELECT COUNT(*) FROM T_version) AS nb_version, (SELECT COUNT(*) FROM T_Type) AS nb_type, (SELECT COUNT(*) FROM T_Sous_titre) AS nb_sous_titre, (SELECT COUNT(*) FROM T_Realisateur) AS nb_realisateur, (SELECT COUNT(*) FROM T_Pays) AS nb_pays, (SELECT COUNT(*) FROM T_Hadi) AS nb_Hadi, (SELECT COUNT(*) FROM T_Genre) AS nb_genre;
+
+SELECT SUM(myVideoCount) FROM(SELECT COUNT(*) AS myVideoCount FROM T_Video, T_version,T_Type,T_Sous_titre,T_Realisateur,T_Pays,T_Hadi,T_Genre) AS A;
